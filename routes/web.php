212e9e357eb1d990ccd105f6a30a0e94375403dc
Matheus <matheusmartinscia@gmail.com>
2025-06-30 20:38:05 -0300

    Feat: Cria rota para método somente users

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::view('/', 'home')->name('home');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/only_admin', [AuthController::class, 'onlyAdmins'])->name('only_admin');
+Route::get('/only_user', [AuthController::class, 'onlyUsers'])->name('only_user');
