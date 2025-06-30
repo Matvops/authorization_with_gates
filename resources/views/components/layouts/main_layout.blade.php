49fedde1a1fdfe20814fedab0e4321cbb1cbf92d
Matheus <matheusmartinscia@gmail.com>
2025-06-29 21:48:08 -0300

    Feat: Preparando projeto

---
+++ resources/views/components/layouts/main_layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gates</title>
+    <link rel="stylesheet" href="{{asset('assets/bootstrap/bootstrap.min.css')}}">
+</head>
+<body>
+    
+    <div class="container mt-3">
+        <p class="display-6">Laravel Gates</p>
+        <hr>
+
+        @auth
+            <p><span class="text-info me-3">{{ Auth::user()->username }}</span><a href="#">Logout</a></p>
+        @else
+            <p>Nenhum usuário logado</p>
+        @endauth
+    </div>
+
+    {{$content}}
+    
+    <script src="{{asset('assets/bootstrap/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
