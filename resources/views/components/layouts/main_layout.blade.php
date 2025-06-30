681998bea63477893cf1fd2a2a02de345c7ff5dd
Matheus <matheusmartinscia@gmail.com>
2025-06-30 18:28:16 -0300

    Feat: Adiciona processo básico de login e logout, nas views

--- resources/views/components/layouts/main_layout.blade.php
+++ resources/views/components/layouts/main_layout.blade.php
@@ -14,7 +14,7 @@
         <hr>
 
         @auth
-            <p><span class="text-info me-3">{{ Auth::user()->username }}</span><a href="#">Logout</a></p>
+            <p><span class="text-info me-3">{{ Auth::user()->name }}</span><a href="{{route('logout')}}">Logout</a></p>
         @else
             <p>Nenhum usuário logado</p>
         @endauth
