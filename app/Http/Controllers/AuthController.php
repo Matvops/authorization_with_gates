83fc97c483f83a2549dd875d8cd5a472ccaad849
Matheus <matheusmartinscia@gmail.com>
2025-06-30 21:00:36 -0300

    Feat: Adiciona not ok aos elses

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 {
     public function login(): RedirectResponse
     {
-        $user = User::find(1);
+        $user = User::find(2);
         Auth::login($user);
         return redirect()->route('home');
     }
@@ -28,16 +28,13 @@ class AuthController extends Controller
     {
         //Auth::user()->can('user_is_admin');
         if(Gate::allows('user_is_admin')) echo "Bem-Vindo admin";
-        else echo "User não é um ADMIN";
+        else echo "NOK2";
     }
 
     public function onlyUsers()
     {
-        /* if(Gate::allows('user_is_user')) echo "Bem-vindo user";
-        else echo "User não é user"; */
-
         //Auth::user()->cannot('user_is_admin');
         if(Gate::denies('user_is_admin')) echo "Bem-vindo user";
-        else echo "Usuário é um admin";
+        else echo "NOK";
     }
 }
