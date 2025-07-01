4b12fc794be8f840db4021c9504eb00687e53791
Matheus <matheusmartinscia@gmail.com>
2025-06-30 21:00:55 -0300

    Feat: Adiciona diretivas blade para can, cannot e canany

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,33 @@
                         @guest
                             <li><a href="{{route('login')}}">Login</a></li>
                         @else
-                            <li><a href="{{route('only_admin')}}">ADMINS</a></li>
-                            <li><a href="{{route('only_user')}}">USERS</a></li>
+                            @can('user_is_admin')
+                                <li><a href="{{route('only_admin')}}">ADMINS</a></li>
+                            @endcan
+                            
+                            @can('user_is_user')
+                                <li><a href="{{route('only_user')}}">USERS</a></li>
+                            @endcan
+
+                            @cannot('user_is_admin')
+                                <div class="row">
+                                    <div class="col">
+                                        <p class="text-danger">FOGO AOS ADMINS!!</p>
+                                    </div>
+                                </div>
+                            @endcannot
+
+                            @cannot('user_is_user')
+                                <div class="row">
+                                    <div class="col">
+                                        <p class="text-danger">OPRESSÃO AOS USERS!!</p>
+                                    </div>
+                                </div>
+                            @endcannot
+
+                            @canany(['user_is_admin', 'user_is_user'])
+                                <p>paz e amor</p>
+                            @endcanany
                         @endguest
                     </ul>
                 </div>
